e37ef767f7f2c87c45c6341b39a3d300ba5b93df
SingleVrUser <SingleVrUser@163.com>
2023-07-01 13:34:28 +0800

    fix some bug

--- Tests/UserInfo.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Tests
-{
-    internal class UserInfo
-    {   
-        public const int UserId = 343058794;
-        public const string UserKey = "8D0513D8D2B91BE22FD7E1612D4BECE69F3DCD95";
-        public const string Cookie = "UID=343058794_P1_1688181289;SEID=aa14c68084bf809ee112b2d816257f5565cd0d0bb2f1a0e230be067f9223ad6327ed963b8f0c2b5b59e14371fe1fec6a196c91864ed08600e836fed2;CID=85a9f96c04156be033f6a500da53b54c;user_id=343058794;";
-    }
-}
